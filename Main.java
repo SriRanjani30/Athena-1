1810a6455dadc22927577b3126e8bd0698472c0a
SriRanjani30 <Sriranjani935@gmail.com>
2025-08-13 05:23:36 +0000

    Sentence Manipulation program using Java ArrayList

--- Main.java
+++ Main.java
@@ -2,19 +2,55 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a sentence to seperate its words as unique: ");
-        String s=sc.nextLine();
-        s.replaceAll("\\?","");
-        String[] stringArray=s.split(" ");
-        ArrayList<String> x=new ArrayList<>(Arrays.asList(stringArray));
-        ArrayList<String> unique=new ArrayList<>();
-        for(String word:x){
-            if(!unique.contains(word)){
-                unique.add(word);
+        while(true){
+            System.out.println("================================");
+            System.out.println("1.Seperate words from sentence\n2.Convert sentence to lowercase\n3.Convert sentence to uppercase\n4.Exit");
+            System.out.println("================================");
+            System.out.println("Enter your choice:");
+            int ch=sc.nextInt();
+            sc.nextLine();
+            switch(ch){
+                case 1:
+                System.out.println("Enter a sentence to seperate its words as unique: ");
+                String s=sc.nextLine();
+                s=s.replaceAll("[^a-zA-Z ]","");
+                String[] stringArray=s.split("\\s+");
+                ArrayList<String> x=new ArrayList<>(Arrays.asList(stringArray));
+                ArrayList<String> unique=new ArrayList<>();
+                for(String word:x){
+                     if(!unique.contains(word)){
+                         unique.add(word);
+                      }
+                 }
+                 System.out.println("After seperating unique words: ");
+                System.out.println(unique);
+                System.out.println();
+                break;
+
+                case 2:
+                System.out.println("Enter a sentence to convert it to lowercase:");
+                String lowerCase=sc.nextLine();
+                System.out.println("Converting to lowercase....");
+                lowerCase=lowerCase.toLowerCase();
+                System.out.println(lowerCase);
+                System.out.println();
+                break;
+
+                case 3:
+                System.out.println("Enter the sentence to convert it to uppercase:");
+                String upperCase=sc.nextLine();
+                System.out.println("Converting to uppercase....");
+                upperCase=upperCase.toUpperCase();
+                System.out.println(upperCase);
+                System.out.println();
+                break;
+
+                case 4:
+                System.out.println("Thank you!");
+                return;
+
             }
+
         }
-        System.out.println(unique);
-
-
     }
 }
